1f6f4c79baeb0524de511afaf2858dde9f7ee333
rohan-3078 <rohan3078@gmail.com>
2024-08-29 00:57:18 +0530

    new

--- ARRAY/rotateToRightToKTimes.py
+++ ARRAY/rotateToRightToKTimes.py
@@ -5,8 +5,11 @@ for i in range(n):
  value=int(input())
  list1.append(value)
 for i in range (k):
- last=list1[n-1]
- for j in range(n-1,0,-1):
-   list1[j]=list1[j-1]
- list1[0] = last
-print(list1)
+ new = list1[n-1:n]+list1[0:n-1]
+ list1 = new
+#  last=list1[n-1]
+#  for j in range(n-1,0,-1):
+#    list1[j]=list1[j-1]
+#  list1[0] = last
+# print(list1)
+print(new)
